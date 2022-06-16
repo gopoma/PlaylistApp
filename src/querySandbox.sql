b61909cc9be6185f3b820a3499cc00c200bc1b0b
Gustavo <gordono@unsa.edu.pe>
2022-06-16 12:34:44 -0500

    Added the queries used in last functionalities

--- src/querySandbox.sql
+++ src/querySandbox.sql
@@ -27,6 +27,7 @@ SELECT * FROM playlists_songs;
 SELECT * FROM users;
 SELECT * FROM songs;
 
+SELECT * FROM playlists;
 SELECT songs.id, songs.title, songs.description, songs.thumbnail, songs.path,
 users.id AS publisher_id, users.username AS publisher_username
 FROM playlists_songs 
@@ -34,7 +35,7 @@ JOIN songs
 ON playlists_songs.id_song=songs.id 
 JOIN users
 ON songs.publisher=users.id
-WHERE playlists_songs.id_playlist=8;
+WHERE playlists_songs.id_playlist=14;
 
 SELECT id, name, id IN (SELECT playlists.id FROM playlists_songs JOIN playlists ON playlists_songs.id_playlist=playlists.id WHERE id_song=1) AS containsSong  FROM playlists WHERE owner=44;
 
@@ -51,3 +52,32 @@ VALUES (1, 2);
 SELECT * FROM playlists WHERE owner=45;
 SELECT id_song FROM playlists_songs WHERE id_playlist=1;
 SELECT * FROM songs WHERE id IN (SELECT id_song FROM playlists_songs WHERE id_playlist=1);
+
+SELECT * FROM songs;
+SELECT * FROM users;
+UPDATE users
+SET role=10
+WHERE id=45;
+UPDATE songs SET genre="other" WHERE id>0;
+SELECT songs.id, songs.publisher, songs.title, songs.description, songs.thumbnail, songs.public, songs.path, songs.genre,
+users.id AS publisher_id, users.username AS publisher_username
+FROM songs 
+JOIN users 
+ON songs.publisher=users.id ORDER BY songs.id DESC;
+
+SELECT songs.id, songs.publisher, songs.title, songs.description, songs.thumbnail, songs.public, songs.path, songs.genre,
+users.id AS publisher_id, users.username AS publisher_username
+FROM songs 
+JOIN users
+ON songs.publisher=users.id
+WHERE songs.genre="electronic"
+ORDER BY songs.id DESC;
+
+SELECT songs.id, songs.publisher, songs.title, songs.description, songs.thumbnail, songs.public, songs.path, songs.genre,
+users.id AS publisher_id, users.username AS publisher_username
+FROM songs 
+JOIN users
+ON songs.publisher=users.id
+WHERE songs.title LIKE "%a%"
+ORDER BY songs.id DESC;
+SELECT * FROM songs;
